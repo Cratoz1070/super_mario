5a24217e7bf7b7b074a0a55920acabb530fd50a6
swd2k <yan.cratoz1977@gmail.com>
2025-12-12 16:53:22 +0300

    Add third level

--- src/levels/third_level.cpp
+++ src/levels/third_level.cpp
@@ -1,24 +1,23 @@
-#include "second_level.hpp"
 #include "third_level.hpp"
 
-using biv::SecondLevel;
+using biv::ThirdLevel;
 
-SecondLevel::SecondLevel(UIFactory* ui_factory) : GameLevel(ui_factory) {
+ThirdLevel::ThirdLevel(UIFactory* ui_factory) : GameLevel(ui_factory) {
 	init_data();
 }
 
-biv::GameLevel* SecondLevel::get_next() {
-	if (!next) {
-		clear_data();
-		next = new biv::ThirdLevel(ui_factory);
-	}
+bool ThirdLevel::is_final() const noexcept {
+	return true;
+}
+
+biv::GameLevel* ThirdLevel::get_next() {
 	return next;
 }
 
 // ----------------------------------------------------------------------------
 // 									PROTECTED
 // ----------------------------------------------------------------------------
-void SecondLevel::init_data() {
+void ThirdLevel::init_data() {
 	ui_factory->create_mario({39, 10}, 3, 3);
 	
 	ui_factory->create_ship({20, 25}, 40, 2);
@@ -33,6 +32,6 @@ void SecondLevel::init_data() {
 
 	ui_factory->create_enemy({85, 20}, 3, 2);
 	ui_factory->create_enemy({150, 20}, 3, 2);
-
-	ui_factory->create_ship({210, 20}, 10, 7);
+    
+    ui_factory->create_ship({210, 20}, 10, 7);
 }
